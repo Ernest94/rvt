79dd80ad8f05822f99d8f82df28a1b43b230fcde
Niels Boer <boertjeniels@gmail.com>
2020-10-09 12:11:54 +0200

    added return

--- J2EE/src/main/java/nu/educom/rvt/rest/ConceptResource.java
+++ J2EE/src/main/java/nu/educom/rvt/rest/ConceptResource.java
@@ -10,19 +10,19 @@ import nu.educom.rvt.services.ConceptService;
 @Path("/webapi/Concept")
 public class ConceptResource {
   private ConceptService conceptServ;
-  
+
   public ConceptResource() {
     this.conceptServ = new ConceptService();
   }
-  
+
   @POST
   @Path("/saveTheme")
   @Consumes(MediaType.APPLICATION_JSON)
   public Response saveTheme(Theme theme) {
     this.conceptServ.addTheme(theme);
-    
-    
+
+    return Response.status(200).entity(theme).build();
   }
-  
+
 
 }
