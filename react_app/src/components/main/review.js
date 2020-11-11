fee1f03174b39256e5849e04a796273b6288fa6c
Ella van der Maas <ellaeducom@outlook.com>
2020-11-11 09:12:36 +0100

    Added sliders for rating and weeks

--- react_app/src/components/main/review.js
+++ react_app/src/components/main/review.js
@@ -6,7 +6,81 @@ import Rating from '@material-ui/lab/Rating';
 import './review.css'
 
 import { config } from '../constants';
+import { FormControlLabel, FormGroup, Checkbox,Slider, Button } from '@material-ui/core';
 
+class ConceptSelection extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state ={
+            stars: [1,5],
+            weeks: [1,8],
+        }
+
+    }
+    handleSliderChange(newValue, name){
+        this.setState({[name]: newValue})
+    }
+    render()
+    {
+    return (
+    <div class={"conceptSelection " + this.props.className} >
+        {this.props.children}
+        <SliderSelection 
+            title="Rating tussen"
+            value={this.state.stars} 
+            handleChange={this.handleSliderChange.bind(this)}
+            handleChangeCommit={this.props.handleChange.bind(this)}
+            min={0}
+            max={5} 
+            name = "stars" 
+        />
+        <SliderSelection 
+            title="Weken"
+            value={this.state.weeks} 
+            handleChange={this.handleSliderChange.bind(this)}
+            handleChangeCommit={this.props.handleChange.bind(this)}
+            min={1}
+            max={8} 
+            name = "weeks" 
+            />
+    </div>
+        )
+    }
+}
+class SliderSelection extends React.Component {
+
+    render (){
+      return (
+        <div >
+            <div><h5 className="selectionTitle" id={this.props.name + "-slider"} >{this.props.title}</h5>
+                <Button 
+                    onClick={()=> {this.props.handleChange([this.props.min,this.props.max],this.props.name);
+                        this.props.handleChangeCommit([this.props.min,this.props.max],this.props.name)}} 
+                    size="small" 
+                    value="All" 
+                    className="lightButton">all
+                </Button>
+            </div>
+            <Slider
+                classes={{thumb: 'sliderThumb' ,
+                        valueLabel: 'sliderLabel' }}
+                name={this.props.name}
+                value={this.props.value}
+                step={1}
+                marks
+                onChange={(e, newValue) => this.props.handleChange(newValue, this.props.name)}
+                onChangeCommitted={(e,newValue) => this.props.handleChangeCommit(newValue, this.props.name)}
+                valueLabelDisplay="auto"
+                aria-labelledby={this.props.name + "-slider"}
+                valueLabelDisplay="on"
+                min={this.props.min}
+                max={this.props.max}
+            />
+        </div>
+        )
+      }
+  }
 class review extends React.Component {
     
     constructor(props) {
@@ -20,7 +94,11 @@ class review extends React.Component {
             pageLoading: false,
             weeksPerBlock: 2,
             errors: "",
-            traineeFeedback: ""
+            traineeFeedback: "",
+            themes: [],
+            starsSelected: [1,5], //starting selection
+            weeksSelected: [1,8],
+            themesSelected: [], //is filled in getThemes
         };
     }
 
@@ -46,10 +124,20 @@ class review extends React.Component {
             await this.setState({ userId: params.userId });
         }
         this.getConcepts();
+        this.getThemes();
         console.log(this.state.userId);
         this.setState({ pageLoading: false });
     }
     
+    getThemes() {
+        axios.get(config.url.API_URL + "/webapi/theme_concept/themes")
+            .then(response => {            
+                this.handleThemeResponse(response.data);
+            })
+            .catch((error) => {
+                console.log("an error occorured " + error);
+            });       
+    }
 
     getConcepts() {
         console.log(this.createUserIdJson());
@@ -61,7 +149,16 @@ class review extends React.Component {
                 console.log("an error occorured " + error);
             });
     }
-
+    handleSelectionChange(newValue, name)
+    {
+        this.setState({[name+"Selected"]: newValue});
+    }
+    handleCheckChange(e)
+    {
+        if(!e.target.checked){
+            this.state.themesSelected.push(e.target.name);
+        } 
+    }
     createUserIdJson() {
         return {
             id: this.state.userId,
@@ -76,6 +173,18 @@ class review extends React.Component {
             concepts: data.conceptsPlusRatings,
         });
         console.log(this.state);
+    }
+    handleThemeResponse(data) {
+        this.selection=[];
+   
+        for(var i=0; i<data.length; i++){
+            this.selection.push({id: data[i].id,checked:true});
+        }
+        this.setState({
+            themes: data,
+            themesSelected:this.selection},()=>{console.log(this.state.themesSelected)
+        })
+
     }
     
     getActiveDisplayName(bool) {
@@ -92,6 +201,16 @@ class review extends React.Component {
             case 5: return ("Uitstekend");
             default: return ("");
         }
+    }
+    inSelection(concept) {
+        let index = this.state.themesSelected.findIndex((obj) => obj.id == concept.concept.theme.id);
+        return(
+            this.state.starsSelected[0] <= concept.rating && concept.rating <= this.state.starsSelected[1]
+            &&
+            this.state.weeksSelected[0] <= concept.concept.week && concept.concept.week <= this.state.weeksSelected[1]
+            &&
+            this.state.themesSelected[index].checked == true
+        )
     }
 
     getWeekBlock(week) {
@@ -110,10 +229,21 @@ class review extends React.Component {
     render() {
         console.log(this.state);
         const { pageLoading, traineeFeedback } = this.state;
+        
         if (pageLoading) return (<span className="center">Laden...</span>)
-
-        var conceptDisplay = this.state.concepts.map((concept) => {
+            
+        var themeSelection = this.state.themes.map((theme) => {
             return (
+                <FormControlLabel
+                control={<Checkbox name={"theme_" + theme.id} onChange={(e) => this.handleCheckChange(e,theme.id)} />}
+                label={theme.abbreviation}
+                />
+            )
+        });
+        var conceptDisplay = this.state.concepts.map((concept) => {
+            if (this.inSelection(concept)){
+            return (
+            
                 <tr>
                     <td className="week">
                         {this.getWeekBlock(concept.concept.week)}
@@ -145,12 +275,14 @@ class review extends React.Component {
                             </TextareaAutosize> 
                     </td> 
                 </tr >
-            )
+                )
+            }
         });
 
         
         return (
                 <div className="container">
+                    
                     <div class="row">
                     <h2 class="col-md-4">{this.state.reviewDate}</h2>
                     <h2 class="col-md-4">Review {this.state.userName}</h2>
@@ -159,6 +291,10 @@ class review extends React.Component {
                     <div >
                         <ul className="errors">{this.state.errors}</ul>                 
                     </div >
+                    
+                    <ConceptSelection className="col-md-4" handleChange={this.handleSelectionChange.bind(this)}>
+                        {themeSelection}
+                    </ConceptSelection>
                     <table >
                         <thead>
                             <tr>
