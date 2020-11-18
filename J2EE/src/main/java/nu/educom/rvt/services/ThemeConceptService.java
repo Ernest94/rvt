a4aded82a69fc2e8026d14a92d94a06ea125a750
bob <mathijskloes@protonmail.com>
2020-11-18 15:29:23 +0100

    made BundleTrainee table and rep. Also added code to change the way concepts are being loaded

--- J2EE/src/main/java/nu/educom/rvt/services/ThemeConceptService.java
+++ J2EE/src/main/java/nu/educom/rvt/services/ThemeConceptService.java
@@ -4,17 +4,22 @@ import java.util.List;
 
 import nu.educom.rvt.models.Concept;
 import nu.educom.rvt.models.Theme;
+import nu.educom.rvt.models.TraineeActive;
 import nu.educom.rvt.models.User;
 import nu.educom.rvt.repositories.ConceptRepository;
 import nu.educom.rvt.repositories.ThemeRepository;
+import nu.educom.rvt.repositories.TraineeActiveRepository;
+
 
 public class ThemeConceptService {
 	private ConceptRepository conceptRepo;
 	private ThemeRepository themeRepo;
+	private TraineeActiveRepository traineeActiveRepo;
 
 	public ThemeConceptService() {
 		this.conceptRepo = new ConceptRepository();
 		this.themeRepo = new ThemeRepository();
+		this.traineeActiveRepo = new TraineeActiveRepository();
 	}
   
 	public Theme addTheme(Theme theme) {
@@ -35,8 +40,15 @@ public class ThemeConceptService {
 		return concepts;
 	}
 	
-	public List<Concept> getAllActiveConceptsfromUser(User user){
-		return this.conceptRepo.readAll();
+//	public List<Concept> getAllActiveConceptsfromUser(User user){
+//		return this.conceptRepo.readAll();// hier moeten de filters over gezet worden of hij in de actieve bundel zit oftewel dat hij specifiek voor die user op actief is gezet in de TraineeActive tabel
+//	}
+	
+	public List<TraineeActive> getAllActiveConceptsFromUser(User user) {
+		return this.traineeActiveRepo.readAll();
 	}
+	
+	
+	
 }
 	
