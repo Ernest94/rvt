d4b59a78ec8b2d06bdb9f2cafd379e470f49d2b2
bob <mathijskloes@protonmail.com>
2020-11-18 12:32:03 +0100

    added bundle and bundleconcept tables and repositories

--- J2EE/src/main/java/nu/educom/rvt/models/Bundle.java
+++ J2EE/src/main/java/nu/educom/rvt/models/Bundle.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="bundle")
-public class bundle {
+public class Bundle {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -22,13 +22,45 @@ public class bundle {
 		super();
 	}
 	
-	public Bundle(Bundle bundle, String name, String startDate, String endDate) {
+	public Bundle(String name, String startDate, String endDate) {
 		super();
-		this.bundle = bundle;
 		this.name = name;
-
 		this.startDate = startDate;
 		this.endDate = endDate;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	
+	
 	
 }
