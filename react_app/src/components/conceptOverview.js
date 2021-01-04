00d6ac0f54085dcbaea55e4d1c069c0988b51a89
Ernest Janssen <janssen.ernest@gmail.com>
2021-01-04 14:45:47 +0100

    when concept is not selected the weekdropdown should be disabled

--- react_app/src/components/conceptOverview.js
+++ react_app/src/components/conceptOverview.js
@@ -184,7 +184,7 @@ class conceptOverview extends React.Component {
                                     value={weekoffset}
                                     onChange={this.onChangeWeek}
                                     required
-                                    disabled={sessionStorage.getItem("userRole")!=="Admin"&&sessionStorage.getItem("userName")!==this.state.selectedBundleCreator}>
+                                    disabled={!selected||sessionStorage.getItem("userRole")!=="Admin"&&sessionStorage.getItem("userName")!==this.state.selectedBundleCreator}>
                                     <option hidden value=''></option>
                                     {weekOptions}
                         </select>
